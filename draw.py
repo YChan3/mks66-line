f7e5920303f01880783aaab3638e910921540350
Derek Chan <ychan3@stuy.edu>
2019-02-10 11:59:46 -0500

    fin

--- draw.py
+++ draw.py
@@ -1,67 +1,95 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    if x0 > x1:
+        store = x1
+        x1 = x0
+        x0 = store
+        store = y1
+        y1 = y0
+        y0 = store
+
+    # print("x0: {}".format(x0))
+    # print("y0: {}".format(y0))
+    # print("x1: {}".format(x1))
+    # print("y1: {}".format(y1))
+
     A = y1 - y0
     B = -1 * (x1 - x0)
 
-    slope = float(A) / (-1 * B)
-    print(slope)
-
-    if slope > 0:
-        if slope <= 1:
-
-            d = A + A + B
-
-            x = x0
-            y = y0
-
-            while x <= x1:
-                plot(screen, color, x, y)
-                if d>0:
-                    y += 1
-                    d += 2 * B
-                x += 1
-                d += 2 * A
+    if B == 0:
+        if y0 > y1:
+            while y0 > y1:
+                plot(screen, color, x0, y0)
+                y0 -= 1
         else:
-            d = A + B + B
+            while y0 < y1:
+                plot(screen, color, x0, y0)
+                y0 += 1
+    elif A == 0:
+        while x0 < x1:
+            plot(screen, color, x0, y0)
+            x0 += 1
 
-            x = x0
-            y = y0
+    else:
+        slope = float(A) / (-1 * B)
+        if slope > 0:
+            if slope <= 1:
 
-            while y <= y1:
-                plot(screen, color, x, y)
-                if d<0:
+                d = A + A + B
+
+                x = x0
+                y = y0
+
+                while x <= x1:
+                    plot(screen, color, x, y)
+                    if d>0:
+                        y += 1
+                        d += 2 * B
                     x += 1
                     d += 2 * A
-                y += 1
-                d += 2 * B
-    else:
-        if (-1 * slope) <= 1:
+            else:
+                d = A + B + B
 
-            d = A + A + B
+                x = x0
+                y = y0
 
-            x = x0
-            y = y0
-
-            while x <= x1:
-                print(d)
-                plot(screen, color, x, y)
-                if d < 0:
-                    y += -1
-                    d += -2 * B
-                x += 1
-                d += 2 * A
+                while y <= y1:
+                    plot(screen, color, x, y)
+                    if d<0:
+                        x += 1
+                        d += 2 * A
+                    y += 1
+                    d += 2 * B
         else:
-            d = A + B + B
+            if (-1 * slope) <= 1:
 
-            x = x0
-            y = y0
+                d = A + A + B
 
-            while y <= y1:
-                plot(screen, color, x, y)
-                if d>0:
-                    x += -1
-                    d += -2 * A
-                y += 1
-                d += 2 * B
+                x = x0
+                y = y0
+
+                while x <= x1:
+                    # print(d)
+                    plot(screen, color, x, y)
+                    if d < 0:
+                        y += -1
+                        d += -2 * B
+                    x += 1
+                    d += 2 * A
+            else:
+                d = A + B + B
+
+                x = x0
+                y = y0
+                # print(A)
+                # print(B)
+
+                while y >= y1:
+                    # print(d)
+                    plot(screen, color, x, y)
+                    if d<0:
+                        x += 1
+                        d += -2 * A
+                    y += -1
+                    d += 2 * B
--- main.py
+++ main.py
@@ -1,12 +1,91 @@
 from display import *
 from draw import *
+import random
+import math
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 0, 0 ]
+val = 0.0
+cycle = 0
 
-# draw_line(0, 0, 500, 100, screen, color)
-# draw_line(0, 0, 100, 500, screen, [255, 0, 0])
-draw_line(0, 100, 500, 0, screen, color)
+INCREMENT = 255/333.33
+
+# draw_line(250, 250, 500, 350, screen, color)
+# draw_line(250, 250, 350, 500, screen, color)
+# draw_line(250, 250, 500, 150, screen, color)
+# draw_line(250, 250, 350, 0, screen, color)
+# draw_line(250, 250, 150, 500, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 0, 350, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 0, 150, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 150, 0, screen, [ 255, 0, 0 ])
+
+# draw_line(250, 250, 0, 250, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 250, 500, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 500, 250, screen, [ 255, 0, 0 ])
+# draw_line(250, 250, 250, 0, screen, [ 255, 0, 0 ])
+
+def color_change(color):
+    global cycle
+    global val
+    if cycle == 0:
+        val += INCREMENT
+        color[2] = math.floor(val)
+        if color[2] == 255:
+            cycle += 1
+            val = 255.0
+    elif cycle == 1:
+        val -= INCREMENT
+        color[0] = math.ceil(val)
+        if color[0] == 0:
+            cycle += 1
+            val = 0.0
+    elif cycle == 2:
+        val += INCREMENT
+        color[1] = math.ceil(val)
+        if color[1] == 255:
+            cycle += 1
+            val = 255.0
+    elif cycle == 3:
+        val -= INCREMENT
+        color[2] = math.ceil(val)
+        if color[2] == 0:
+            cycle += 1
+            val = 0.0
+    elif cycle == 4:
+        val += INCREMENT
+        color[0] = math.ceil(val)
+        if color[0] == 255:
+            cycle += 1
+            val = 255.0
+    elif cycle == 5:
+        val -= INCREMENT
+        color[1] = math.ceil(val)
+
+ex = 0
+ey = 500
+
+while ex <= 500:
+    draw_line(500, 0, ex, ey, screen, color)
+    ex += 1
+    color_change(color)
+
+ex = 500
+while ey >= 0:
+    draw_line(500, 0, ex, ey, screen, color)
+    ey += -1
+    color_change(color)
+
+ey = 0
+while ex >= 0:
+    draw_line(500, 0, ex, ey, screen, color)
+    ex += -1
+    color_change(color)
+
+ex = 0
+while ey <= 500:
+    draw_line(500, 0, ex, ey, screen, color)
+    ey += 1
+    color_change(color)
 
 display(screen)
 save_extension(screen, 'img.png')
